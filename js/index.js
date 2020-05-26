2bd7f33cfe21d76241c1ce7c9f2b8f73b2898261
Andrey Trifonenko <sintez91@gmail.com>
2020-05-26 15:16:47 +0500

    manual draw

--- js/index.js
+++ js/index.js
@@ -6,6 +6,8 @@ let cols;
 let rows;
 let field = [];
 let gen;
+let isManual = false;
+let isStarted = false;
 
 const input_sizeCell = document.getElementById('sizeCell');
 const input_density = document.getElementById('density');
@@ -18,6 +20,7 @@ document.getElementById('l_density').textContent = `Плотность: ${input_
 function inputChange(id) {
   if (id == 'sizeCell') {
     sizeCell = document.getElementById(id).value;
+    initNewField();
   } else {
     density = document.getElementById(id).value;
   }
@@ -29,34 +32,55 @@ function inputChange(id) {
 function resizeCanvas() {
   canvas.width = document.querySelector('.game-field').clientWidth;
   canvas.height = document.querySelector('.game-field').clientHeight;
-  cols = Math.floor(canvas.width / sizeCell);
-  rows = Math.floor(canvas.height / sizeCell);
+  initNewField();
 }
 resizeCanvas();
 
-function start() {
-  let random;
-  context.clearRect(0, 0, canvas.width, canvas.height);
+function initNewField() {
+  field = [];
+  cols = Math.floor(canvas.width / sizeCell);
+  rows = Math.floor(canvas.height / sizeCell);
   for (let x = 0; x < cols; x++) {
     field[x] = [];
     for (let y = 0; y < rows; y++) {
-      random = Math.floor(Math.random() * density);
-      if (random == 0) {
-        field[x][y] = true;
-        context.fillStyle = 'rgb(255,0,0)';
-        context.fillRect(x * sizeCell, y * sizeCell, sizeCell, sizeCell);
-      } else field[x][y] = false;
+      field[x][y] = false;
+    }
+  }
+}
+
+function start() {
+  if (isStarted) return;
+  if (!isManual) {
+    let random;
+    for (let x = 0; x < cols; x++) {
+      for (let y = 0; y < rows; y++) {
+        random = Math.floor(Math.random() * density);
+        if (random == 0) {
+          field[x][y] = true;
+          context.fillStyle = 'rgb(255,0,0)';
+          context.fillRect(x * sizeCell, y * sizeCell, sizeCell, sizeCell);
+        }
+      }
     }
   }
   input_sizeCell.setAttribute('disabled', true);
   input_density.setAttribute('disabled', true);
   gen = setInterval(iteration, 100);
+  isStarted = true;
 }
 
 function stop() {
   clearInterval(gen);
+  isStarted = false;
+  isManual = false;
+  context.clearRect(0, 0, canvas.width, canvas.height);
   input_sizeCell.removeAttribute('disabled');
   input_density.removeAttribute('disabled');
+}
+
+function pause() {
+  clearInterval(gen);
+  isStarted = false;
 }
 
 function iteration() {
@@ -83,7 +107,6 @@ function countOFLivingNeighbors(col, row) {
     for (let y = -1; y < 2; y++) {
       let chekingX = (x + col + cols) % cols;
       let chekingY = (y + row + rows) % rows;
-
       if (x == 0 && y == 0) continue;
       if (field[chekingX][chekingY]) {
         count++;
@@ -103,4 +126,31 @@ function redraw() {
       }
     }
   }
+}
+
+function getMousePos(e) {
+  let canvasRect = canvas.getBoundingClientRect();
+  return {
+    x: e.clientX - canvasRect.left,
+    y: e.clientY - canvasRect.top,
+  };
+}
+
+function manualDraw(e) {
+  isManual = true;
+  let pos = getMousePos(e);
+  let xCellPos = Math.floor(pos.x / sizeCell);
+  let yCellPos = Math.floor(pos.y / sizeCell);
+
+  input_sizeCell.setAttribute('disabled', true);
+  input_density.setAttribute('disabled', true);
+
+  if (field[xCellPos][yCellPos]) {
+    context.fillStyle = 'rgb(0,0,0)';
+    field[xCellPos][yCellPos] = false;
+  } else {
+    context.fillStyle = 'rgb(255,0,0)';
+    field[xCellPos][yCellPos] = true;
+  }
+  context.fillRect(xCellPos * sizeCell, yCellPos * sizeCell, sizeCell, sizeCell);
 }
